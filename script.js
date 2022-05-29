9fc68a2885e04fea2dc066dec86e9dfc5f3be0ff
alexisrw33 <69729270+alexisrw33@users.noreply.github.com>
2022-05-28 19:04:20 -0500

    added sliding pages

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const SliderContainer = document.querySelector('.slider-container')
+const leftSlide = document.querySelector('.left-slide')
+const rightSlide = document.querySelector('.right-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const SliderContainer = document.querySelector('.slider-container')
+const slideLength = rightSlide.querySelectorAll('div')
 
+let activeSlideIndex = 0
+
+leftSlide.style.top = `-${(slideLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = SliderContainer.clientHeight
+    if (direction === 'up') {
+        activeSlideIndex++
+        if (activeSlideIndex > slideLength - 1) {
+            activeSlideIndex = 0
+        }
+    }
+    else if (direction === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slideLength - 1
+        }
+    }
+
+    rightSlide.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    leftSlide.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+}
